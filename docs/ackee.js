e5dacc4034e6075d9dc131dd234b9c23e9a46e44
Marek Jędryka <jedryka89@gmail.com>
2021-06-24 00:23:48 +0200

    Added DOMContentLoaded event support

--- docs/ackee.js
+++ docs/ackee.js
@@ -1,6 +1,10 @@
-const s = document.createElement('script')
-s.async = true
-s.src = 'https://maje-ackee.herokuapp.com/tracker.js'
-s.dataset.ackeeServer = 'https://maje-ackee.herokuapp.com'
-s.dataset.ackeeDommainId = '91f48a1b-da2a-41e6-9854-7e88d44a15a5'
-document.body.append(s)
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log({ event })
+  const s = document.createElement('script')
+  s.async = true
+  s.src = 'https://maje-ackee.herokuapp.com/tracker.js'
+  s.dataset.ackeeServer = 'https://maje-ackee.herokuapp.com'
+  s.dataset.ackeeDommainId = '91f48a1b-da2a-41e6-9854-7e88d44a15a5'
+  document.body.append(s)
+})
+
